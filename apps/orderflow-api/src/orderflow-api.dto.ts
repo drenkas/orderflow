1c69d74b74d1c04a95063c2e8232b65b046e1468
focus1691 <mrjoshuaali@yahoo.co.uk>
2023-12-01 02:35:30 +0000

    Updated build files

--- apps/orderflow-api/src/orderflow-api.dto.ts
+++ apps/orderflow-api/src/orderflow-api.dto.ts
@@ -1,11 +1,10 @@
 /* eslint-disable indent */
-import { Exchange } from '@orderflow-service/constants/exchanges'
 import { INTERVALS } from '@orderflow-api/constants.ts'
 import { IsString, IsEnum } from 'class-validator'
 
 export class CandleBySymbolAndIntervalDto {
   @IsString()
-  exchange: Exchange
+  exchange: string
 
   @IsString()
   symbol: string
