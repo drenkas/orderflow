f1fa341be6bac084d7cdba0b1a4a9e3304916f8a
Joshua <mrjoshuaali@yahoo.co.uk>
2024-09-06 00:54:35 +0100

    Update bybit module

--- apps/bybit/src/bybit.module.ts
+++ apps/bybit/src/bybit.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { ScheduleModule } from '@nestjs/schedule'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { AppService } from './app.service'
 import { DatabaseModule } from '@database'
-import { ORMConfig } from '@database/ormconfig'
-import { ByBitService } from 'apps/bybit/src/bybit.service'
-import { BybitWebSocketService } from 'apps/bybit/src/BybitWebsocketService'
+import { DatabaseConfiguration } from '@database/ormconfig'
+import { ByBitService } from './bybit.service'
+import { BybitWebSocketService } from './BybitWebsocketService'
 
 @Module({
-  imports: [ConfigModule.forRoot({}), ScheduleModule.forRoot(), TypeOrmModule.forRoot(ORMConfig), DatabaseModule],
-  providers: [AppService, ByBitService, BybitWebSocketService]
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ScheduleModule.forRoot(), TypeOrmModule.forRoot(DatabaseConfiguration), DatabaseModule],
+  providers: [ByBitService, BybitWebSocketService]
 })
-export class BinanceModule {}
+export class BybitModule {}
