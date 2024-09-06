6923e33cea288fe7259aa4f13ae85d24c3473dd4
Joshua <mrjoshuaali@yahoo.co.uk>
2024-09-06 01:05:15 +0100

    bybit update

--- apps/bybit/src/BybitWebsocketService.ts
+++ apps/bybit/src/BybitWebsocketService.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common'
 import { Observable, Subject } from 'rxjs'
-import { TradeData, TradeResponse } from 'apps/bybit/src/websocket.responses'
+import { TradeData, TradeResponse } from './websocket.responses'
 import { CategoryV5, WebsocketClient } from 'bybit-api'
 
 @Injectable()
 export class BybitWebSocketService {
   private ws: WebsocketClient
   private tradeUpdates$: Subject<TradeData[]> = new Subject()
+  private reconnect$: Subject<string> = new Subject()
+  private connected$: Subject<string> = new Subject()
 
   constructor() {
     this.initWebSocket()
@@ -18,10 +20,29 @@ export class BybitWebSocketService {
 
   private initWebSocket(): void {
     this.ws = new WebsocketClient({ market: 'v5' })
+
     this.ws.on('update', (response: TradeResponse) => {
       if (response.topic.startsWith('publicTrade')) {
         this.tradeUpdates$.next(response.data as TradeData[])
       }
+    })
+
+    this.ws.on('open', (event) => {
+      console.log('connection opened:', event.wsKey, event.ws.target.url)
+      this.connected$.next(event.wsKey)
+    })
+
+    this.ws.on('reconnect', (data) => {
+      console.log('ws automatically reconnecting.... ', data?.wsKey)
+    })
+
+    this.ws.on('reconnected', (data) => {
+      console.log('ws has reconnected ', data?.wsKey)
+      this.reconnect$.next(data.wsKey)
+    })
+
+    this.ws.on('error', (data) => {
+      console.log('ws saw error ', data?.wsKey)
     })
   }
 
