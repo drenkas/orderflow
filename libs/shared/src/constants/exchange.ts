66e7ee348b2815c3fc4bf3d7d7a726efdfafcf41
focus1691 <mrjoshuaali@yahoo.co.uk>
2024-11-24 14:39:47 +0000

    remove EXCHANGE_DATA_TYPES

--- libs/shared/src/constants/exchange.ts
+++ libs/shared/src/constants/exchange.ts
@@ -4,16 +4,6 @@ export enum Exchange {
   BINANCE = 'binance',
   BITMEX = 'bitmex',
   BYBIT = 'bybit',
+  BITGET = 'bitget',
   OKX = 'okx'
-}
-
-export enum EXCHANGE_DATA_TYPES {
-  STOCKS_CHART = 'stocks',
-  SYMBOLS = 'symbols',
-  KLINES = 'kline',
-  FOOTPRINT_CANDLES = 'footprint_candle',
-  OPEN_INTEREST = 'open_interest',
-  VOLUME_RATIO = 'volume_ratio',
-  LATEST_BIG_DEAL = 'latest_big_deal',
-  FUNDING_RATE = 'funding_rate'
 }
